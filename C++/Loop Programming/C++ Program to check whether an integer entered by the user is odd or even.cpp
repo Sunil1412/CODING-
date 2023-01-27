37d63cab4dfb3f0c9c478a39babfad26e1ad7fb7
SUNIL <86763715+Sunil1412@users.noreply.github.com>
2023-01-27 18:25:11 +0530

    Update C++ Program to check whether an integer entered by the user is odd or even.cpp

--- C++/Loop Programming/C++ Program to check whether an integer entered by the user is odd or even.cpp
+++ C++/Loop Programming/C++ Program to check whether an integer entered by the user is odd or even.cpp
@@ -1,2 +1,36 @@
-//C++ Program to check whether an integer entered by the user is odd or even
+ //Write C++ program to print alphabets from a to z
 
+// for loop
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    char i;
+    
+    for(i='a';i<='z';i++)
+    {
+        cout<<i<<endl;
+        
+    }
+   
+    return 0;
+    
+}
+
+//*************************************While loop**********************************************************
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    char i='a';
+    while(i<='z'){
+        
+        
+        cout<<i++<<endl;
+    }
+    
+        return 0;
+}
