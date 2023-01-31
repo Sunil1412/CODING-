31092eeb9ce912d853ac6712e54fc572586899c2
SUNIL <86763715+Sunil1412@users.noreply.github.com>
2023-01-31 23:28:06 +0530

    Create Write C++ program to print all natural numbers in reverse order.cpp

---
+++ C++/Loop Programming/Write C++ program to print all natural numbers in reverse order.cpp
@@ -0,0 +1,17 @@
+//Write C++ program to print all natural numbers in reverse order
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+int i,n;
+cin>>n;
+    
+    for(i=n;i>=1;i--){
+            cout<<i<<endl;
+            
+        }
+        
+return 0;
+}
